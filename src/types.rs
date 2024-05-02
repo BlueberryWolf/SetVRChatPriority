2161bd2d21c7e64bffe86ba50e4cfec967cd6ca9
Krystian <31629699+Kawaxte@users.noreply.github.com>
2024-05-02 06:01:53 +0300

    that fucking `licenser` extension automated wrong licence

--- src/types.rs
+++ src/types.rs
@@ -1,6 +1,18 @@
-// Copyright (c) 2024 Kawaxte
-// 
-// This software is released under the MIT License.
-// https://opensource.org/licenses/MIT
+// Copyright (C) 2024 Kawaxte
+//
+// This file is part of vrc-priority-rs.
+//
+// vrc-priority-rs is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// vrc-priority-rs is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with vrc-priority-rs.  If not, see <https://www.gnu.org/licenses/>.
 
 pub(super) type Throwable<T> = Result<T, Box<dyn std::error::Error>>;
